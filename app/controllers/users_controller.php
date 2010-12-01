6d81159926dba3070cc92c94cd28f93aa195a562
Vanessa Gutiérrez <vanecgs@gmail.com>
2010-12-01 18:01:28 -0200

    added admin methods for backend

--- app/controllers/users_controller.php
+++ app/controllers/users_controller.php
@@ -1,10 +1,10 @@
 <?php
 class UsersController extends AppController {
+
 	var $name = 'Users';
-	var $scaffold = 'admin';
 	
 	var $components = array('Auth','RequestHandler');
-
+		
     function beforeFilter() {
 		
         $this->Auth->fields = array(
@@ -14,49 +14,126 @@ class UsersController extends AppController {
 		
 		parent::beforeFilter();
 		
-		$this->Auth->loginRedirect = array('controller' => 'users', 'action' => 'view');
+		$this->Auth->loginRedirect = array('controller' => 'users', 'action' => 'view', $this->Auth->user('cid'));
 		
-		$this->Auth->allow('add','isAuth');
+		$this->Auth->allow('login','isAuth');
+		
     }
+	
+	
 
 	function login($id = null) {
-		if($id) {
-			$this->redirect(array('controller' => 'schools', 'action' => 'info', $id));
-		}
     }
 
     function logout() {
         $this->redirect($this->Auth->logout());
     }
 	
-	function add() {
-		if (!empty($this->data)) {
-			if ($this->User->save($this->data)) {
-				$this->Session->setFlash('User has been saved.');
-				$this->redirect(array('controller' => 'home', 'action' => 'index'));
-			}
-		}
-	}
-	
-	function view() {
-		$this->set('user', $this->User->read(array('first_name','last_name','cid'),$this->Session->read('Auth.User.cid')));
-	}
-	
-	function edit($id = null) {
-		$this->User->id = $id;
-		if (empty($this->data)) {
-			$this->data = $this->User->read();
-		} else {
-			if ($this->User->save($this->data)) {
-				$this->Session->setFlash('User has been updated.');
-				$this->redirect(array('action' => 'view'));
-			}
-		}
-	}
-	
-	function isAuth() {
-		$this->layout = 'ajax';
+
+	function index() {		
+		$this->redirect(array('action' => 'view', $this->Auth->user('cid')));
+		//$this->User->recursive = 0;
+		//$this->set('users', $this->paginate());
 	}
 
+	function view($id = null) {
+		if (!$id) {
+			$this->Session->setFlash(__('Invalid user', true));
+			$this->redirect(array('action' => 'index'));
+		}
+		$this->set('user', $this->User->read(null, $id));
+	}
+
+	function edit($id = null) {
+		if (!$id && empty($this->data)) {
+			$this->Session->setFlash(__('Invalid user', true));
+			$this->redirect(array('action' => 'index'));
+		}
+		if (!empty($this->data)) {
+			if ($this->User->save($this->data)) {
+				$this->Session->setFlash(__('The user has been saved', true));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The user could not be saved. Please, try again.', true));
+			}
+		}
+		if (empty($this->data)) {
+			$this->data = $this->User->read(null, $id);
+		}
+	}
+
+	function delete($id = null) {
+		if (!$id) {
+			$this->Session->setFlash(__('Invalid id for user', true));
+			$this->redirect(array('action'=>'index'));
+		}
+		if ($this->User->delete($id)) {
+			$this->Session->setFlash(__('User deleted', true));
+			$this->redirect(array('action'=>'index'));
+		}
+		$this->Session->setFlash(__('User was not deleted', true));
+		$this->redirect(array('action' => 'index'));
+	}
+
+	function admin_index() {
+		if($this->Auth->user('group_id') == 1) {
+			$this->User->recursive = 0;
+			$this->set('users', $this->paginate());	
+		}
+		else {
+			$this->redirect(array('controller' => 'users', 'action' => 'view', $this->Auth->user('cid')));
+		}
+	}
+
+	function admin_view($id = null) {
+		if (!$id) {
+			$this->Session->setFlash(__('Invalid user', true));
+			$this->redirect(array('action' => 'index'));
+		}
+		$this->set('user', $this->User->read(null, $id));
+	}
+
+	function admin_add() {
+		if (!empty($this->data)) {
+			$this->User->create();
+			if ($this->User->save($this->data)) {
+				$this->Session->setFlash(__('The user has been saved', true));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The user could not be saved. Please, try again.', true));
+			}
+		}
+	}
+
+	function admin_edit($id = null) {
+		if (!$id && empty($this->data)) {
+			$this->Session->setFlash(__('Invalid user', true));
+			$this->redirect(array('action' => 'index'));
+		}
+		if (!empty($this->data)) {
+			if ($this->User->save($this->data)) {
+				$this->Session->setFlash(__('The user has been saved', true));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The user could not be saved. Please, try again.', true));
+			}
+		}
+		if (empty($this->data)) {
+			$this->data = $this->User->read(null, $id);
+		}
+	}
+
+	function admin_delete($id = null) {
+		if (!$id) {
+			$this->Session->setFlash(__('Invalid id for user', true));
+			$this->redirect(array('action'=>'index'));
+		}
+		if ($this->User->delete($id)) {
+			$this->Session->setFlash(__('User deleted', true));
+			$this->redirect(array('action'=>'index'));
+		}
+		$this->Session->setFlash(__('User was not deleted', true));
+		$this->redirect(array('action' => 'index'));
+	}
 }
 ?>
