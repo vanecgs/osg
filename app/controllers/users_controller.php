d35347cb9cd20ebaa223b340e97d2302de62551b
Vanessa Gutiérrez <vanecgs@gmail.com>
2010-12-01 18:25:48 -0200

    fixed redirection error, apparently

--- app/controllers/users_controller.php
+++ app/controllers/users_controller.php
@@ -81,59 +81,79 @@ class UsersController extends AppController {
 			$this->set('users', $this->paginate());	
 		}
 		else {
-			$this->redirect(array('controller' => 'users', 'action' => 'view', $this->Auth->user('cid')));
+			$this->redirect('/users/view/'.$this->Auth->user('cid'));
 		}
 	}
 
 	function admin_view($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(__('Invalid user', true));
-			$this->redirect(array('action' => 'index'));
+		if($this->Auth->user('group_id') == 1) {
+			if (!$id) {
+				$this->Session->setFlash(__('Invalid user', true));
+				$this->redirect(array('action' => 'index'));
+			}
+			$this->set('user', $this->User->read(null, $id));
 		}
-		$this->set('user', $this->User->read(null, $id));
+		else {
+			$this->redirect('/users/view/'.$this->Auth->user('cid'));
+		}
 	}
 
 	function admin_add() {
-		if (!empty($this->data)) {
-			$this->User->create();
-			if ($this->User->save($this->data)) {
-				$this->Session->setFlash(__('The user has been saved', true));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The user could not be saved. Please, try again.', true));
+		if($this->Auth->user('group_id') == 1) {
+			if (!empty($this->data)) {
+				$this->User->create();
+				if ($this->User->save($this->data)) {
+					$this->Session->setFlash(__('The user has been saved', true));
+					$this->redirect(array('action' => 'index'));
+				} else {
+					$this->Session->setFlash(__('The user could not be saved. Please, try again.', true));
+				}
 			}
+		}
+		else {
+			$this->redirect('/users/view/'.$this->Auth->user('cid'));
 		}
 	}
 
 	function admin_edit($id = null) {
-		if (!$id && empty($this->data)) {
-			$this->Session->setFlash(__('Invalid user', true));
-			$this->redirect(array('action' => 'index'));
-		}
-		if (!empty($this->data)) {
-			if ($this->User->save($this->data)) {
-				$this->Session->setFlash(__('The user has been saved', true));
+		if($this->Auth->user('group_id') == 1) {
+			if (!$id && empty($this->data)) {
+				$this->Session->setFlash(__('Invalid user', true));
 				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The user could not be saved. Please, try again.', true));
+			}
+			if (!empty($this->data)) {
+				if ($this->User->save($this->data)) {
+					$this->Session->setFlash(__('The user has been saved', true));
+					$this->redirect(array('action' => 'index'));
+				} else {
+					$this->Session->setFlash(__('The user could not be saved. Please, try again.', true));
+				}
+			}
+			if (empty($this->data)) {
+				$this->data = $this->User->read(null, $id);
 			}
 		}
-		if (empty($this->data)) {
-			$this->data = $this->User->read(null, $id);
+		else {
+			$this->redirect('/users/view/'.$this->Auth->user('cid'));
 		}
 	}
 
 	function admin_delete($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(__('Invalid id for user', true));
-			$this->redirect(array('action'=>'index'));
+		if($this->Auth->user('group_id') == 1) {
+			if (!$id) {
+				$this->Session->setFlash(__('Invalid id for user', true));
+				$this->redirect(array('action'=>'index'));
+			}
+			if ($this->User->delete($id)) {
+				$this->Session->setFlash(__('User deleted', true));
+				$this->redirect(array('action'=>'index'));
+			}
+			$this->Session->setFlash(__('User was not deleted', true));
+			$this->redirect(array('action' => 'index'));
 		}
-		if ($this->User->delete($id)) {
-			$this->Session->setFlash(__('User deleted', true));
-			$this->redirect(array('action'=>'index'));
+		else {
+			$this->redirect('/users/view/'.$this->Auth->user('cid'));
 		}
-		$this->Session->setFlash(__('User was not deleted', true));
-		$this->redirect(array('action' => 'index'));
 	}
 }
 ?>
